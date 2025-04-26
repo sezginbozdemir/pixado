b3a3c135843e5a27f061344b6faddee9c977b738
sezginbozdemir <88796191+sezginbozdemir@users.noreply.github.com>
2025-04-27 02:07:59 +0300

    fix scroll aniamtion

--- src/components/home/scroll-animation/ScrollAnimation.tsx
+++ src/components/home/scroll-animation/ScrollAnimation.tsx
@@ -10,53 +10,21 @@ const ScrollMask = () => {
     target: ref,
   });
 
-  const scale = useTransform(scrollYProgress, [0, 1], [90, 5]);
+  const scale = useTransform(scrollYProgress, [0, 1], [15, 1]);
   const opacity = useTransform(scrollYProgress, [0.2, 0.45], [0, 1]);
-
-  const scaleSmallScreen = useTransform(scrollYProgress, [0, 1], [90, 4]);
-  const opacitySmallScreen = useTransform(scrollYProgress, [0.2, 0.45], [0, 1]);
   const isSmallScreen = useMediaQuery({ maxWidth: 767 });
 
   return (
     <div ref={ref} className="scroll-mask-container">
       <div className="scroll-content">
-        <motion.div
-          style={{ scale: isSmallScreen ? scaleSmallScreen : scale }}
-          className="mask-container"
-        >
-          <svg className="text-mask" width="100%" height="100%">
-            <defs>
-              <mask id="textMask">
-                <motion.rect
-                  style={{
-                    opacity: isSmallScreen ? opacitySmallScreen : opacity,
-                  }}
-                  width="100%"
-                  height="100%"
-                  fill="white"
-                />
-                <motion.text
-                  style={{
-                    opacity: isSmallScreen ? opacitySmallScreen : opacity,
-                  }}
-                  x="50%"
-                  y="50%"
-                  dominantBaseline="middle"
-                  textAnchor="middle"
-                  fill="#091100"
-                >
-                  PIXADO
-                </motion.text>
-              </mask>
-            </defs>
-            <rect
-              width="100%"
-              height="100%"
-              fill="#091100"
-              mask="url(#textMask)"
-            />
-          </svg>
-        </motion.div>
+        <motion.img
+          style={{
+            opacity: opacity,
+            scale: scale,
+          }}
+          src={isSmallScreen ? `/pixado-mobile.png` : `/pixado.png`}
+          className="text-mask"
+        />
       </div>
     </div>
   );
